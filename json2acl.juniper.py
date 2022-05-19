fe3a44dffa6570643b1436d4a7faa671bc319aac
Liviu Pislaru <liviu@duiadns.net>
2022-05-19 22:34:58 +0300

    Update json2acl.juniper.py
    Added size to juniper firewall

--- json2acl.juniper.py
+++ json2acl.juniper.py
@@ -39,7 +39,18 @@ for key in data.keys():
         d_ports = eval(d_port.replace('~', ''))
         for dp in d_ports:
             print(f"set firewall family inet filter fw-ddos term {key} from destination-port {dp}")
-
+    # match size range
+    if (size != '*'):
+        x = size.split(",")
+        if (x[0].startswith("(")):
+            s1 = int(x[0][1:]) + 1
+        else:
+            s1 = int(x[0][1:])
+        if (x[1].endswith(")")):
+            s2 = int(x[1][0:int(len(x[1])) - 1]) - 1
+        else:
+            s2 = int(x[1][0:int(len(x[1])) - 1])
+        print(f"set firewall family inet filter fw-ddos term {key} from packet-length [{s1} {s2}]")
     print(f"set firewall family inet filter fw-ddos term {key} then count {key}")
     print(f"set firewall family inet filter fw-ddos term {key} then discard")
 
